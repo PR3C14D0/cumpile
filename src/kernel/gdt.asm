13692a0662f7d8972ec232fc693f84af1cf69452
Preciado <aarongarpre@gmail.com>
2026-05-21 01:25:59 +0200

    feat: gdt_flush function created

---
+++ src/kernel/gdt.asm
@@ -0,0 +1,17 @@
+global gdt_flush
+
+gdt_flush:
+    mov eax, [esp + 4]
+    lgdt[eax]
+
+    mov ax, 0x10
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov fs, ax
+    mov gs, ax
+
+    jmp 0x08:.flush
+.flush:
+    ret
+
